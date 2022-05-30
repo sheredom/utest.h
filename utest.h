2acb361391fecfb49a3c8c96ae5f70b4f116e21f
frithrah <wizard@frithrah.com>
2022-05-30 16:48:56 +0100

    Suppress MSVC C4820 warning (alignment padding)

--- utest.h
+++ utest.h
@@ -497,7 +497,8 @@ utest_type_printer(long long unsigned int i) {
 #define UTEST_SURPRESS_WARNING_BEGIN                                           \
   __pragma(warning(push))                                                      \
   __pragma(warning(disable : 4127))                                            \
-  __pragma(warning(disable : 4571))
+  __pragma(warning(disable : 4571))                                            \
+  __pragma(warning(disable : 4820))
 #define UTEST_SURPRESS_WARNING_END __pragma(warning(pop))
 #else
 #define UTEST_SURPRESS_WARNING_BEGIN
