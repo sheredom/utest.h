9f8b2a8da19712bfe69896e74946b1fdf5724494
Facundo Galan <facundogalan91@gmail.com>
2023-08-20 02:54:10 -0300

    Replace EXPECT_STRNE and ASSERT_STRNE with UTEST_STRNE

--- utest.h
+++ utest.h
@@ -846,7 +846,7 @@ utest_strncpy_gcc(char *const dst, const char *const src, const size_t size) {
 #define EXPECT_STREQ(x, y) UTEST_STREQ(x, y, 0)
 #define ASSERT_STREQ(x, y) UTEST_STREQ(x, y, 1)
 
-#define EXPECT_STRNE(x, y)                                                     \
+#define UTEST_STRNE(x, y, is_assert)                                           \
   UTEST_SURPRESS_WARNING_BEGIN do {                                            \
     const char *xEval = (x);                                                   \
     const char *yEval = (y);                                                   \
@@ -856,10 +856,14 @@ utest_strncpy_gcc(char *const dst, const char *const src, const size_t size) {
       UTEST_PRINTF("  Expected : \"%s\"\n", xEval);                            \
       UTEST_PRINTF("    Actual : \"%s\"\n", yEval);                            \
       *utest_result = UTEST_TEST_FAILURE;                                      \
+      if (is_assert) return;                                                   \
     }                                                                          \
   }                                                                            \
   while (0)                                                                    \
   UTEST_SURPRESS_WARNING_END
+
+#define EXPECT_STRNE(x, y) UTEST_STRNE(x, y, 0)
+#define ASSERT_STRNE(x, y) UTEST_STRNE(x, y, 1)
 
 #define EXPECT_STRNEQ(x, y, n)                                                 \
   UTEST_SURPRESS_WARNING_BEGIN do {                                            \
@@ -967,22 +971,6 @@ utest_strncpy_gcc(char *const dst, const char *const src, const size_t size) {
   while (0)                                                                    \
   UTEST_SURPRESS_WARNING_END
 #endif
-
-#define ASSERT_STRNE(x, y)                                                     \
-  UTEST_SURPRESS_WARNING_BEGIN do {                                            \
-    const char *xEval = (x);                                                   \
-    const char *yEval = (y);                                                   \
-    if (UTEST_NULL == xEval || UTEST_NULL == yEval ||                          \
-        0 == strcmp(xEval, yEval)) {                                           \
-      UTEST_PRINTF("%s:%i: Failure\n", __FILE__, __LINE__);                    \
-      UTEST_PRINTF("  Expected : \"%s\"\n", xEval);                            \
-      UTEST_PRINTF("    Actual : \"%s\"\n", yEval);                            \
-      *utest_result = UTEST_TEST_FAILURE;                                      \
-      return;                                                                  \
-    }                                                                          \
-  }                                                                            \
-  while (0)                                                                    \
-  UTEST_SURPRESS_WARNING_END
 
 #define ASSERT_STRNEQ(x, y, n)                                                 \
   UTEST_SURPRESS_WARNING_BEGIN do {                                            \
